99cb52d4476fc02308ffb72497889b26df360fcd
Howard <howardl93@yahoo.com>
2019-12-24 05:51:57 -0800

    setting up

--- server.js
+++ server.js
@@ -15,19 +15,38 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+//tests 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
 
+app.get("/", (req, res) => res.send("Hello World!!"));
 
 
 
 // database 
 //can replace with db const 
-const DATABASEurl =  process.env.MONGODB_URI ||  'mongodb://localhost:27017/persons';
+// const DATABASEurl =  process.env.MONGODB_URI ||  'mongodb://localhost:27017/persons';
 
-mongoose.connect(DATABASEurl, { useNewUrlParser: true });
+mongoose.connect(db, { useNewUrlParser: true });
 const connection = mongoose.connection;
 connection.once('open', function() {
     console.log("MongoDB database connection established successfully");
-})
+});
+
+
+//deployment -- might not need to use cuz concurrently but always good to have as referece 
+
+
+app.use(express.static(path.join(__dirname, "frontend", "build")));
+
+
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+});
+
+
+//routes 
+
