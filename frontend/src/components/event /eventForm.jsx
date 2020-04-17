efab4422010cb26e3f08e8afa3f5082c783a7ecf
Howard <howardl93@yahoo.com>
2020-04-17 03:15:32 -0700

    event component

--- frontend/src/components/event /eventForm.jsx
+++ frontend/src/components/event /eventForm.jsx
@@ -6,15 +6,44 @@ import React from 'react';
 
 const eventForm = () =>{
 
+
+
+
+
+	const createEvent =(e) =>{
+		e.preventDefault();
+		//api call to backend 
+
+		alert("an event was created!");
+
+
+	}
+
 	return (
 		<div className="event-form">
-			<form>
+			<form onSubmit{e => createEvent()}>
 			<fieldset>
 
+				<datepicker>
+				</datepicker>
 
+				<input  type="email"
+                placeholder="Where should I remind you?"
+                id="email"
+                value={email}
+                onChange={e => setEmail(e.target.value)}/>
+
+
+                <input 
+                type="text"
+                placeholder="What are you up to?"
+                id="title"
+                value={title}
+                onChange={e => setTitle(e.target.value)}
+              />
+
+              
 			</fieldset>
-
-
 			</form>
 
 		</div>
@@ -23,6 +52,8 @@ const eventForm = () =>{
 
 
 };
+
+
 
 
 export default eventForm;
