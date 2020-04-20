5fe774fbfda0cf7de990b4cc2505f44554ae3654
Howard <howardl93@yahoo.com>
2020-04-20 00:44:42 -0700

    adding event modal component

--- frontend/src/components/event/eventmodal.jsx
+++ frontend/src/components/event/eventmodal.jsx
@@ -1,11 +1,38 @@
 //eventmodal.jsx
 
 import React from 'react';
+import {Modal} from '@material-ui/core/Modal';
 
+import EventForm from './eventform';
 
 
 class EventModal extends React.Component{
+	constructor(props){
+		super(props);
+		state = {
+			title: "",
+		}
+	}
 
+
+	render(){
+		return(
+			<Modal
+				open={open}
+  				onClose={handleClose}
+  				>
+
+
+
+
+  			<EventForm/>
+
+
+  			
+  			</Modal>
+
+			)
+	}
 
 
 };
