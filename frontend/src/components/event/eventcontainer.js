145e07d034a05d9d0dcfe3a3c4ff36d772acb579
Howard <howardl93@yahoo.com>
2020-05-07 21:16:06 -0700

    event actions

--- frontend/src/components/event/eventcontainer.js
+++ frontend/src/components/event/eventcontainer.js
@@ -3,7 +3,7 @@
 
 import {connect} from "react-redux";
 
-import {createEvent} from '../../actions/event_actions';
+import {createEvent, updateEvent, deleteEvent} from '../../actions/event_actions';
 
 import EventForm from './eventform';
 
@@ -19,7 +19,8 @@ const mapStateToProps = (state) => ({
 
 
 const mapDispatchToProps = dispatch => ({
-	createEvent: (data) => dispatch(createEvent(data))
+	createEvent: (data) => dispatch(createEvent(data)),
+
 });
 
 
