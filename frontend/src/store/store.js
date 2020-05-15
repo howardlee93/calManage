d37a2b8284898e71e6931df812999da6e655d63a
Howard <howardl93@yahoo.com>
2020-05-15 00:48:37 -0700

    redux devtools

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,15 +1,26 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
 
 import rootReducer from '../reducers/root_reducer';
 
+
+
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+
 const configureStore = (preloadedState = {}) => (
   createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunk, logger)
+
+    composeEnhancers(applyMiddleware(thunk, logger))
   )
 );
 
