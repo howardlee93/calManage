d8f5f19db816bc4e9727da29beef95514fe3df4b
Howard <howardl93@yahoo.com>
2020-06-03 15:09:07 -0700

    adding reducers

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -3,9 +3,11 @@
 import { combineReducers } from 'redux';
 import eventReducer from './event_reducer';
 import calReducer from './cal_reducer';
+import sessionReducer from './session_reducer';
+
 
 export default combineReducers({
 	events: eventReducer,
-	// calendar: calReducer
+	session: sessionReducer
 
 })
