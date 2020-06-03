92bebe64698515d1187118ea04354df038136463
Howard <howardl93@yahoo.com>
2020-06-03 15:10:35 -0700

    simplifed user model

--- models/user_model.js
+++ models/user_model.js
@@ -3,10 +3,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-	username: {
-		type: String,
-     	required: true
-    },
     email: {
       	type: String,
       	required: true
@@ -15,10 +11,7 @@ const userSchema = new Schema({
       	type: String,
       	required: true
     },
-    date: {
-      	type: Date,
-      	default: Date.now
-    }
+
 });
 
 
